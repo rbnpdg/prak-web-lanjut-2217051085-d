1b80cd603d25437567289cd073204d6b54d26fb6
rbnpdg <aditamarubenpandega@gmail.com>
2024-09-08 23:06:55 +0700

    Tambah routing

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,15 @@
+<tr>
+    <td>Nama</td>
+    <td> :</td>
+    <td><?= $nama ?></td>
+</tr><br>
+<tr>
+    <td>Kelas</td>
+    <td> :</td>
+    <td><?= $kelas ?></td>
+</tr><br>
+<tr>
+    <td>NPM</td>
+    <td> :</td>
+    <td><?= $npm ?></td>
+</tr>
